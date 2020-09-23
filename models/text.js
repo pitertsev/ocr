9298bfbef8e2d31d9d566bdb237caeb5de0f0b43
Andrey Kretsu <lutch97@gmail.com>
2020-09-23 15:01:55 +0300

    added recognition of voice without function that we need

--- models/text.js
+++ models/text.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const textSchema = new mongoose.Schema({
   text: String,
-});
+})
 
-module.exports = mongoose.model('Text', textSchema);
+module.exports = mongoose.model('Text', textSchema)
