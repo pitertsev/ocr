7fefd448d02224dc0baace973da3eba78b67267e
Alexander Glazyrin <alexander.ig.glazyrin@gmail.com>
2020-09-23 12:28:06 +0300

    added form tesseract

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const userMiddleware = require('./middleware')
 
 const indexRouter = require('./routes/index')
+const authRouter = require('./routes/auth')
 
 const app = express()
 userMiddleware(app)
 
 app.use('/', indexRouter)
+app.use('/auth', authRouter)
 
 module.exports = app
