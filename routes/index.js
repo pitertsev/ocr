65878ed64f70ebc468bf0eff37252df5215999cb
VanMexico <ivan@pitertsev.pw>
2020-09-23 14:33:45 +0300

    Added tesseract, fileUpload. Read image complete.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router()
+const path = require('path')
+const { readImage } = require('../middleware/tesseract')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', async (req, res) => {
+  const img = req.files.file.data // Buffer
+  // const testFile = path.join(__dirname, '..', 'public', 'img', 'img.png')
+  const text = await readImage(img, 'rus')
+  console.log(text)
 })
 
 module.exports = router
