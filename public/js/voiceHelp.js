9eb4bab641a64e7348808c14ec0c2070fa6e93be
Andrey Kretsu <lutch97@gmail.com>
2020-09-23 17:55:33 +0300

    added help with speak

--- public/js/voiceHelp.js
+++ public/js/voiceHelp.js
@@ -1,14 +1,13 @@
-function help() {
-  if (annyang) {
-    const commands = {
-      Помоги() {
-        alert('help')
-      },
-    }
-    annyang.init(commands)
-    annyang.setLanguage('ru')
-    annyang.start()
-  }
-}
+// function help() {
+//   if (annyang) {
+//     let commands = {
+//     'Помоги': function() {
+//       speak('Выберите картинку, которая содержит текст, который вам необходимо озвучить');
+//     }};
+//     annyang.init(commands);
+//     annyang.setLanguage('ru');
+//     annyang.start();
+//     }
+// }
 
-help()
+// help()
