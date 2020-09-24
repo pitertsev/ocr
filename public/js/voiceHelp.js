649237d5147c4ac6b93111d7ddeda0a9a016d358
Andrey Kretsu <lutch97@gmail.com>
2020-09-24 10:32:21 +0300

    added command when becomes ill

--- public/js/voiceHelp.js
+++ public/js/voiceHelp.js
@@ -1,13 +1,19 @@
 function help() {
   if (annyang) {
     const commands = {
-    'Помоги': function() {
-      speak('Выберите картинку, которая содержит текст, который вам необходимо озвучить');
-    }}
+      'Помоги': function() {
+        speak('Выберите картинку, которая содержит текст, который вам необходимо озвучить');
+      },
+      'Мне плохо': async function () {
+        await fetch('/help')
+          .then(speak('Сообщение отправлено'))
+          .catch(speak('Сообщение не отправлено'))
+      },
+    }
     annyang.init(commands)
     annyang.setLanguage('ru')
     annyang.start()
-    }
+  }
 }
 
 help()
